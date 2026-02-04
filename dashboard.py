7f872d8f8ef955cf5755b00062847fc05c04cacb
KhodrKurdi <khoder.kur123@hotmail.com>
2026-02-04 20:08:11 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -45,7 +45,7 @@ def load_data():
     """Load all datasets"""
     try:
         physicians = pd.read_csv('/mnt/user-data/uploads/Physicians_Indicators_Anonymized.csv')
-        dept_2023 = pd.read_csv('/mnt/user-data/uploads/All_Departments_2023.csv', low_memory=False)
+        dept_2023 = pd.read_csv('All_Departments_2023.csv', low_memory=False)
         dept_2024 = pd.read_csv('/mnt/user-data/uploads/All_Departments_2024.csv', low_memory=False)
         dept_2025 = pd.read_csv('/mnt/user-data/uploads/All_Departments_2025.csv', low_memory=False)
         
